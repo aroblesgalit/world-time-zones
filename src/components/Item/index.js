2659afa13705a5b0c6bf839304ebe9ddd436f1cb
Alvin Galit <aroblesgalit@gmail.com>
2021-05-27 09:26:07 -0500

    Created states for each value of a timezone item

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment-timezone';
 
 export const Item = ({ item }) => {
 
-    // Get name and time zone
-    const value = item;
-    const valueArr = value.split('_');
-    const name = valueArr.join(' ');
-    const newTimeZone = moment().tz(item).format('h:mm:ss a z');
+    const [value] = useState(item);
+    const [name] = useState(() => {
+        let valueArr = value.split('_');
+        return valueArr.join(' ');
+    });
+    const [time, setTime] = useState(() => {
+        return moment().tz(item).format('h:mm:ss a z')
+    });
+
+    // // Get name and time zone
+    // const value = item;
+    // const valueArr = value.split('_');
+    // const name = valueArr.join(' ');
+    // let time;
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(() => {
+                moment().tz(item).format('h:mm:ss a z');
+            });
+        }, 1000)
+    }, [])
 
     return (
-        <div key={`${i}-${item.name}`} className='item'>
-            <p>{item.name}</p>
-            <p>{item.time}</p>
+        <div key={`${i}-${value}`} className='item'>
+            <p>{name}</p>
+            <p>{time}</p>
             <div>&#10006;</div>
         </div>
     )
