d48c24460985837c5cfee67826b2d8ccdb657c72
Alvin Galit <aroblesgalit@gmail.com>
2021-05-26 22:58:39 -0500

    Adjusted padding and borders on list of zones

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([
+    { name: 'America/New York', time: '12:10:15' },
+    { name: 'America/Los Angeles', time: '12:10:15' }
+  ]);
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log('hello')
+    }, 1000)
+  }, [])
+
   return (
     <main>
       <div>
         <h1>World Time Zones</h1>
-        <select name='time-zones' id='time-zones'>
-          <option value='' selected disabled>Add a time zone</option>
+        <select name='time-zones' id='time-zones' defaultValue='Add a time zone'>
           <option value='America/New_York'>America/New York</option>
           <option value='America/Los_Angeles'>America/Los Angeles</option>
           <option value='America/London'>Europe/London</option>
         </select>
-        <div class='list'>
-          <div class='item'>
-            <p>Time zone name</p>
-            <p>12:10:20</p>
-            <div>&#10006;</div>
-          </div>
+        <div className='list'>
+          {
+            items && items.map((item, i) => {
+              return (
+                <div key={`${i}-${item.name}`} className='item'>
+                  <p>{item.name}</p>
+                  <p>{item.time}</p>
+                  <div>&#10006;</div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </main>
