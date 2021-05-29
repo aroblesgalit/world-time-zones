a19aecac3801062998a8e64d0c683913f017536c
Alvin Galit <aroblesgalit@gmail.com>
2021-05-28 22:00:01 -0500

    Added logic for removing timezone from select and adding it back in on delete from table

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Item from './components/Item';
 
 function App() {
 
-  const timezones = [
+  const [timezones, setTimezones] = useState([
     { value: 'America/New_York', name: 'America/New York' },
     { value: 'America/Los_Angeles', name: 'America/Los Angeles' },
     { value: 'Europe/London', name: 'Europe/London' },
     { value: 'Australia/Sydney', name: 'Australia/Sydney' },
     { value: 'Asia/Tokyo', name: 'Asia/Tokyo' },
-  ]
+  ]);
 
   const [items, setItems] = useState([]);
 
@@ -23,13 +23,25 @@ function App() {
 
     // Add to items array
     setItems(prevState => [...prevState, item])
+
+    // Remove item from timezones array
+    const newTimezones = [...timezones].filter(timezone => timezone.value !== item);
+    setTimezones(newTimezones);
   }
 
   function deleteTimeZone(e, i) {
     e.preventDefault();
     const currentItems = [...items];
-    currentItems.splice(i, 1);
+    const item = currentItems.splice(i, 1);
     setItems(currentItems);
+
+    // Add item back into timezones array
+    const newTimezones = [...timezones];
+    newTimezones.push({
+      value: item.join(),
+      name: item.join().split('_').join(' ')
+    })
+    setTimezones(newTimezones);
   }
 
   return (
@@ -37,17 +49,19 @@ function App() {
       <div>
         <h1>World Time Zones</h1>
         <select name='time-zones' id='time-zones' onChange={(e) => addTimeZone(e)}>
-          <option value='' selected disabled>Select a time zone to add</option>
+          <option value='' selected>Select a time zone to add</option>
           {
             timezones.map((timezone, i) => <option key={`${i}-${timezone.value}`} value={timezone.value}>{timezone.name}</option>)
           }
         </select>
         <table className='list'>
-          {
-            items && items.map((item, i) => {
-              return <Item key={`${i}-${item}`} item={item} onDelete={(e) => deleteTimeZone(e, i)} />
-            })
-          }
+          <tbody>
+            {
+              items && items.map((item, i) => {
+                return <Item key={`${i}-${item}`} item={item} onDelete={(e) => deleteTimeZone(e, i)} />
+              })
+            }
+          </tbody>
         </table>
       </div>
     </main>
